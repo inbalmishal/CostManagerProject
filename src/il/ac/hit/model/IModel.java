71af7c3d300f2073299d781781f76e7b70ef10ac
tallevi120 <tallevi120@gmail.com>
2021-01-05 19:35:18 +0200

    fix warnings.

--- src/il/ac/hit/model/IModel.java
+++ src/il/ac/hit/model/IModel.java
@@ -10,80 +10,80 @@ public interface IModel {
     /**
      * This method take CostOrIncome type parameter and adds it to the DB.
      * @param item CostOrIncome type parameter.
-     * @throws CostManagerException
+     * @throws CostManagerException Problem with the DB.
      */
-    public void addCostOrIncome(CostOrIncome item ) throws CostManagerException;
+    void addCostOrIncome(CostOrIncome item) throws CostManagerException;
 
     /**
      * This method take id of CostOrIncome type parameter and deletes it from the DB.
      * @param id The id of CostOrIncome type parameter.
-     * @throws CostManagerException
+     * @throws CostManagerException The id not exist or problem with the DB.
      */
-    public void deleteCostOrIncome(int id) throws CostManagerException;
+    void deleteCostOrIncome(int id) throws CostManagerException;
 
     /**
      * This method take Category type parameter and adds it to the DB.
      * @param item Category type parameter.
-     * @throws CostManagerException
+     * @throws CostManagerException The category already exist or problem with the DB.
      */
-    public void addNewCategory(Category item) throws CostManagerException;
+    void addNewCategory(Category item) throws CostManagerException;
 
     /**
      * This method take Category type parameter and deletes it from the DB.
      * @param item Category type parameter.
-     * @throws CostManagerException
+     * @throws CostManagerException This category doesn't exist or problem with the DB.
      */
-    public void deleteCategory(Category item ) throws CostManagerException;
+    void deleteCategory(Category item) throws CostManagerException;
 
     /**
      * This method take date from and date to and get from the DB all the costs between the dates.
      * @param from The first date we want to see.
      * @param to The last date we want to see.
      * @return Array list with all costs between the dates.
-     * @throws CostManagerException
+     * @throws CostManagerException The date 'from' come after the date 'to'.
      */
-    public ArrayList<CostOrIncome> getAllCostsBetweenDates(Date from,Date to) throws CostManagerException;
+    ArrayList<CostOrIncome> getAllCostsBetweenDates(Date from, Date to) throws CostManagerException;
     /**
      * This method take date from and date to and get from the DB all the incomes between the dates.
      * @param from The first date we want to see.
      * @param to The last date we want to see.
      * @return Array list with all incomes between the dates.
-     * @throws CostManagerException
+     * @throws CostManagerException The date 'from' come after the date 'to'.
      */
-    public ArrayList<CostOrIncome> getAllIncomesBetweenDates(Date from,Date to) throws CostManagerException;
+    ArrayList<CostOrIncome> getAllIncomesBetweenDates(Date from, Date to) throws CostManagerException;
     /**
      * This method take date "from" and date "to" and get from the DB all the costs and incomes between the dates.
      * @param from The first date we want to see.
      * @param to The last date we want to see.
      * @return Array list with all costs and incomes between the dates.
-     * @throws CostManagerException
+     * @throws CostManagerException The date 'from' come after the date 'to'
      */
-    public ArrayList<CostOrIncome> getAllCostsAndIncomesBetweenDates(Date from,Date to) throws CostManagerException;
+    ArrayList<CostOrIncome> getAllCostsAndIncomesBetweenDates(Date from, Date to) throws CostManagerException;
 
     /**
      * This method calculate the balance in my DB.
      * @return The balance in my DB.
-     * @throws CostManagerException
+     * @throws CostManagerException Problem with the DB.
      */
-    public double getTheBalance() throws CostManagerException;
+    double getTheBalance() throws CostManagerException;
     /**
      * This method get from the DB all the costs and incomes.
      * @return Array list with all costs and incomes in my DB.
-     * @throws CostManagerException
+     * @throws CostManagerException Problem with the DB.
      */
-    public ArrayList<CostOrIncome> getAllCostsAndIncomes() throws CostManagerException;
+    ArrayList<CostOrIncome> getAllCostsAndIncomes() throws CostManagerException;
 
     /**
      * This method get from the DB all the categories.
      * @return Array list with all categories in my DB.
-     * @throws CostManagerException
+     * @throws CostManagerException Problem with the DB.
      */
-    public ArrayList<Category> getAllCategories() throws CostManagerException;
+    ArrayList<Category> getAllCategories() throws CostManagerException;
 
     /**
      * This method open at the beginning of the application running and create the tables and default categories,
      * but only if the tables and categories not exists before.
-     * @throws CostManagerException
+     * @throws CostManagerException Problem with the DB.
      */
-    public void createDB()throws CostManagerException;
+    void createDB()throws CostManagerException;
 }
